b31ffc64d7d08cf71fac1ce8f55bfa6059568a51
andrew-melnychuk <58741436+andrew-melnychuk@users.noreply.github.com>
2024-01-24 22:52:24 +0200

    add experience time calculating (#5)

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,12 +1,17 @@
+import { careerStart, millisecondsInYear } from '../constants';
+import { getTimeDiff } from '../utils';
 import { Section } from './index';
 
 const Summary = () => (
   <Section title="summary">
     <div className="text-secondary">
       <p>
-        Energetic individual with 3 years of experience and a track record of
+        {`Energetic individual with more than ${getTimeDiff(
+          careerStart,
+          'year',
+        )} years of experience and a track record of
         delivering high-quality solutions in web development. Have a solid
-        understanding of and experience with SCRUM methodology.
+        understanding of and experience with SCRUM methodology.`}
       </p>
       <p>
         Results-driven team player with well-developed soft skills. Seeking
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,17 @@
+import { millisecondsInMonth, millisecondsInYear } from '../constants';
+
 export { default as usePrint } from './usePrint';
 export * from './useLocalStorage';
 
 export const classNames = (...classes: unknown[]): string =>
   classes.filter(Boolean).join(' ');
+
+export const getTimeDiff = (date: Date, granularity: string) => {
+  const granularityMap = {
+    year: millisecondsInYear,
+    month: millisecondsInMonth,
+  };
+
+  // @ts-ignore
+  return Math.floor(Math.abs(new Date() - date) / granularityMap[granularity]);
+};
