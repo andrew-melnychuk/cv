2f922c2d1e701536470cbb450d2dbd677ec61be9
andrew-melnychuk <58741436+andrew-melnychuk@users.noreply.github.com>
2024-10-08 11:28:31 +0300

    fix: change the experience calculation in summary

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,12 +6,20 @@ export * from './useLocalStorage';
 export const classNames = (...classes: unknown[]): string =>
   classes.filter(Boolean).join(' ');
 
-export const getTimeDiff = (date: Date, granularity: string) => {
+export const getTimeDiff = (date: Date, granularity: 'year' | 'month', round = true) => {
   const granularityMap = {
     year: millisecondsInYear,
     month: millisecondsInMonth,
-  };
+  } as const;
 
-  // @ts-ignore
-  return Math.floor(Math.abs(new Date() - date) / granularityMap[granularity]);
+  const diff = Math.abs(new Date().getTime() - date.getTime()) / granularityMap[granularity]
+
+  return round ? Math.floor(diff) : diff;
 };
+
+export const getExperience = (start: Date) => {
+    const exp = getTimeDiff(start, 'year', false);
+    const decimalPart = exp - Math.floor(exp);
+
+    return decimalPart > 8 ? `${Math.ceil(exp)}` : decimalPart > 5 ? `close to ${Math.ceil(exp)}` : decimalPart > 2 ? `more than ${Math.floor(exp)}` : `${Math.floor(exp)}`;
+} 
