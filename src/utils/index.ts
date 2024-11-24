8a48de0678241d59737a8d4399769afa45211402
andrew melnychuk <anmeln@softserveinc.com>
2024-11-24 12:24:02 +0200

    fix: fix safari exp calculation

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,20 +6,32 @@ export * from './useLocalStorage';
 export const classNames = (...classes: unknown[]): string =>
   classes.filter(Boolean).join(' ');
 
-export const getTimeDiff = (date: Date, granularity: 'year' | 'month', round = true) => {
+export const getTimeDiff = (
+  date: Date,
+  granularity: 'year' | 'month',
+  round = true,
+) => {
   const granularityMap = {
     year: millisecondsInYear,
     month: millisecondsInMonth,
   } as const;
 
-  const diff = Math.abs(new Date().getTime() - date.getTime()) / granularityMap[granularity]
+  const diff =
+    Math.abs(new Date().getTime() - date.getTime()) /
+    granularityMap[granularity];
 
   return round ? Math.floor(diff) : diff;
 };
 
 export const getExperience = (start: Date) => {
-    const exp = getTimeDiff(start, 'year', false);
-    const decimalPart = exp - Math.floor(exp);
+  const exp = getTimeDiff(start, 'year', false);
+  const decimalPart = exp - Math.floor(exp);
 
-    return decimalPart > 0.8 ? `${Math.ceil(exp)}` : decimalPart > 0.5 ? `close to ${Math.ceil(exp)}` : decimalPart > 0.2 ? `more than ${Math.floor(exp)}` : `${Math.floor(exp)}`;
-} 
+  return decimalPart > 0.8
+    ? `${Math.ceil(exp)}`
+    : decimalPart > 0.5
+    ? `close to ${Math.ceil(exp)}`
+    : decimalPart > 0.2
+    ? `more than ${Math.floor(exp)}`
+    : `${Math.floor(exp)}`;
+};
