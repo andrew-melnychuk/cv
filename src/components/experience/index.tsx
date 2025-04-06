ea85706947dc52d0f268291c4b4c18ba849272ea
andrew-melnychuk <58741436+andrew-melnychuk@users.noreply.github.com>
2025-04-06 19:33:31 +0300

    update layout and cv file (#10)

--- src/components/experience/index.tsx
+++ src/components/experience/index.tsx
@@ -1,11 +1,13 @@
 import { Section } from '../common';
 import First from './First';
+import Fourth from './Fourth';
 import Second from './Second';
 import Third from './Third';
 
 const Experience = () => (
   <Section title="experience">
     <Third />
+    <Fourth />
     <Second />
     <First />
   </Section>
